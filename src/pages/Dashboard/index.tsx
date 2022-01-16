8d78e97ec8268a4601d19d0deb5a87dba5369d4e
PaolaGuedes <paolaguedesbacca2002@gmail.com>
2022-01-16 20:39:55 -0300

    input form

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,19 +1,13 @@
 import { useState, useEffect } from 'react';
-import { useLocation, useHistory } from 'react-router-dom';
-import { useForm } from "react-hook-form";
-
+import { useLocation } from 'react-router-dom';
 
 import { api } from '../../services/api';
 
 import { Card, Modal, Input, Form } from 'antd';
 import { EditOutlined } from '@ant-design/icons';
 
-import LayoutBase from '../../components/LayoutBase'
-
-import { AtivosProps } from '../../components/Ativos'
-import { CardProgress } from './CardProgress';
-
 import {Bar} from 'react-chartjs-2'
+
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -24,26 +18,17 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+
 import { CardData } from './CardData';
+import LayoutBase from '../../components/LayoutBase'
+import { AtivosProps } from '../../components/Ativos'
+import { CardProgress } from './CardProgress';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, BarElement, Title, Tooltip, Legend);
-
-interface Ativo {
-  id: number
-  sensors: Array<string>
-  model: string
-  name: string
-}
 
 function Dashboard() {
 
   const { pathname } = useLocation()
-  const history = useHistory()
-
-  // useEffect(() => {
-  //   api.get(`${pathname}`)
-  //     .then(response => setValue(response.data))
-  // });
 
   const [dash, setDash] = useState<AtivosProps>({
     id: 0,
@@ -68,28 +53,21 @@ function Dashboard() {
 
   useEffect(() => {
     api.get(`${pathname}`)
-      .then(response => setDash(response.data))
+      .then(response => {
+        setDash(response.data)
+        setName(dash.name)
+        setModel(dash.model)
+      })
   });
+
+  const [name, setName] = useState('')
+  const [model, setModel] = useState('')
 
   const [isOpen, setIsOpen] = useState(false)
 
   function onEditAtivo() {
     setIsOpen(true)
   }
-
-  // const {register} = useForm<Ativo>({
-  //   id: 0,
-  //   sensors: [],
-  //   model: '',
-  //   name: ''
-  // })
-
-  // function onSubmit(ev: any){
-  //   ev.preventDefault()
-
-  //   api.post(`${pathname}`, value)
-  //   .then((response) => history.push('/'))
-  // }
 
   return (
     <LayoutBase to="/" path={'Inicial / Ativos / ' + dash.name}>
@@ -102,39 +80,32 @@ function Dashboard() {
           onEditAtivo()
         }} style={{fontSize: 18}}/>}
       >
-        <Modal 
+      <Modal 
         title="Editar ativo" 
         okText="Salvar"
         visible={isOpen}
         onCancel={() => setIsOpen(false)}
         onOk={() => setIsOpen(false)}
-        >
-      {/* <Form
+      >
+      <Form
         name="update"
-        onFinish={onSubmit}>
+      >
         <Form.Item label="Nome">
         <Input 
           name="name"
-          value={value.name}
-          onChange={onChange} 
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         </Form.Item>
         <Form.Item label="Modelo">
         <Input 
           name="model"
-          value={value.model}
-          onChange={onChange} 
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
         />
         </Form.Item>
-        <Form.Item label="Sensor">
-        <Input 
-          name="sensors"
-          value={value.sensors}
-          onChange={onChange} 
-        />
-        </Form.Item>
-        </Form> */}
-        </Modal>
+        </Form>
+      </Modal>
 
         <CardData 
           sensors={dash.sensors}
